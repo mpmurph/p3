c42792d0462bdb95aea5bc69351f4914d8c3fd5b
Mary Murphy <marymurphy@g.harvard.edu>
2014-10-16 13:39:17 -0400

    added explanatory comments

--- app/php/fillertextlogic.php
+++ app/php/fillertextlogic.php
@@ -1,12 +1,20 @@
 <?php
 	
+	//only perform the logic if the user has pressed the submit button
 	if (Input::get("submit")) {
 
+		//check the users inputs to make sure they are valid - if not, return warnings
 		require(app_path()."/php/warningslogic.php");
 
+		//if the user's requested number of paragraphs falls within the given parameters,
+		//retrieve the user's request
 		if (Input::get("count") > 0 && Input::get("count") <=1000) {
+
+			//create a new instance of the LoremIpsum class
 			$generator = new LoremIpsum();
 
+			//if the user wants short paragraphs, set the variables of the 
+			//getParagraphs method to reflect this preference
 			if (Input::get("paraglength")=="short") {
 
 				$generator -> setSentenceStDev(1);
@@ -16,6 +24,8 @@
 
 			}
 
+			//if the user wants medium-length paragraphs, set the variables of the 
+			//getParagraphs method to reflect this preference
 			elseif (Input::get("paraglength")=="medium") {
 
 				$generator -> setSentenceStDev(1);
@@ -25,6 +35,8 @@
 
 			}
 
+			//if the user wants long paragraphs, set the variables of the 
+			//getParagraphs method to reflect this preference
 			elseif (Input::get("paraglength")=="long") {
 
 				$generator -> setSentenceStDev(3);
@@ -34,7 +46,11 @@
 
 			}
 
+			//call the getParagraphs method, passing in the number of paragraphs
+			//the user requested
 			$paragraphs = $generator->getParagraphs(Input::get("count"));
+
+			//output the paragraphs
 			echo implode("<p id=\"actualoutput\">", $paragraphs);
 
 		}
--- app/php/randomuserslogic.php
+++ app/php/randomuserslogic.php
@@ -1,35 +1,47 @@
 <?php
 
+	//only perform the logic if the user has pressed the submit button
 	if (Input::get("submit")) {
 
+		//check the users inputs to make sure they are valid - if not, return warnings
 		require(app_path()."/php/warningslogic.php");
 
+		//if the user's requested number of users falls within the given parameters,
+		//retrieve the user's request
 		if (Input::get("count") > 0 && Input::get("count") <=1000) {
 
+			//create a new instance of the Faker class
 			$faker = Faker::create();
 
+			//cycle through a process to generate - do this the same number of times
+			//as the number of users that has been requested
 			for ($i=1; $i<=Input::get("count"); $i++) {
 
+				//generate a user name
 				echo "Name: ".$faker -> name, "<br>";
 
+				//if the user has requested to include birthdays, generate a birthdate
 				if (Input::get("birthday")) {
 
 					echo "Birthday: ".$faker->dateTimeThisCentury->format('Y-m-d'), "<br>";
 
 				}
 
+				//if the user has requested to include a zip code, generate a zip code
 				if (Input::get("zip")) {
 
 					echo "Zip Code: ".$faker -> postcode, "<br>";
 
 				}
 
+				//if the user has requested to include a profile, generate a lorem ipsum profile
 				if (Input::get("profile")) {
 
 					echo "Profile: ".$faker -> text ($maxNbChars = 200), "<br>";
 
 				}
 
+				//create some spacing between each user that is generated
 				echo "<br>";
 				echo "<br>";
 
