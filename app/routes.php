58ff0c7b905d5e3a583385d3d3dcdb74d698bae8
Mary Murphy <marymurphy@g.harvard.edu>
2014-10-18 14:40:06 -0400

    managing variables and implementing classes to get logic out of views

--- app/routes.php
+++ app/routes.php
@@ -11,22 +11,110 @@
 |
 */
 
-//Route::get('/', function()
-//{
-//	return View::make('hello');
-//});
-
 Route::get('/', function()
 {
-	return View::make('welcome');
+	return View::make("welcome");
 });
+
 
 Route::get('/fillertext', function()
 {
-	return View::make('fillertext');
+
+	//render blank page if no data has been submitted
+	if (!Input::get("submit")) {
+
+		return View::make("fillertext");
+
+	}
+
+	$warningcheck = new Warnings();
+
+	$warnings = $warningcheck->errorCheckUserEntry(Input::get("count"));
+
+	if ($warnings) {
+
+		return View::make("fillertext")
+			->with("warnings", $warnings);
+
+	}
+
+	$filler = new Filler();
+
+	$paragraphs = $filler->makeParagraphs(Input::get("paraglength"), Input::get("count"));
+
+	return View::make("fillertext")
+		->with("paragraphs", $paragraphs);
+
 });
+
 
 Route::get('/randomusers', function()
 {
-	return View::make('randomusers');
+	
+	//render blank page if no data has been submitted
+	if (!Input::get("submit")) {
+
+		return View::make("randomusers");
+
+	}
+
+	$warningcheck = new Warnings();
+
+	$warnings = $warningcheck->errorCheckUserEntry(Input::get("count"));
+
+	if ($warnings) {
+
+		return View::make("randomusers")->with("warnings", $warnings);
+
+	}
+
+	$user = new User();
+	$count = Input::get("count");
+
+	$usernames = $user->getName(Input::get("count"));
+
+
+		if(Input::get("birthday")) {
+
+			$userbirthdays = $user->getBirthday(Input::get("count"));
+
+		}
+
+		if(!Input::get("birthday")) {
+
+			$userbirthdays = NULL;
+
+		}
+
+		if(Input::get("zip")) {
+
+			$userzips = $user->getZip(Input::get("count"));
+
+		}
+
+		if(!Input::get("zip")) {
+
+			$userzips = NULL;
+
+		}
+
+		if(Input::get("profile")) {
+
+			$userprofiles = $user->getUserProfile(Input::get("count"));
+
+		}
+
+		if(!Input::get("profile")) {
+
+			$userprofiles = NULL;
+
+		}
+
+	return View::make("randomusers")
+		->with("count", $count)
+		->with("names", $usernames)
+		->with("birthdays", $userbirthdays)
+		->with("zips", $userzips)
+		->with("profiles", $userprofiles);
+
 });
