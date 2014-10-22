a91f364481738219edd24f5c3cd6206ed5c852fb
Mary Murphy <marymurphy@g.harvard.edu>
2014-10-22 10:00:46 -0400

    created error message page to appear when 404 Not Found errors occur

---
+++ app/views/error.blade.php
@@ -0,0 +1,31 @@
+@extends("_master")
+
+@section("title")
+
+	I'm Not Your BFF!
+
+@stop
+
+@section("content")
+
+	<br>
+
+	<div>
+
+		<h1 style="text-align:center">Ruff! Ruff! This is the wrong page!</h1>
+
+		<h3 style="text-align:center">Please click <a href="/">here</a> to return to your BFF!</h3>
+
+	</div>
+
+@stop
+
+@section("footer")
+
+	<footer>
+
+		<img src="images/bffdoggie.jpg" alt="Dogs are BFFs too!" id="footerimage"/>
+
+	</footer>
+
+@stop
